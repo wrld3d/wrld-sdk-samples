fb1194eeb55ae5873080bf03cae7442e71449e77
Mark Simpson <mark.simpson@eegeo.com>
2014-08-14 14:39:11 +0100

    Fix up breaking change in traffic congestion API.
    Buddy: Scott M

--- src/Examples/TrafficCongestion/TrafficCongestionExample.cpp
+++ src/Examples/TrafficCongestion/TrafficCongestionExample.cpp
@@ -14,19 +14,20 @@ const float NumSecondsEachCongestion = 5.f;
 namespace Examples
 {
 TrafficCongestionExample::TrafficCongestionExample(Eegeo::TrafficCongestion::ITrafficCongestionService& trafficCongestionService,
-        Eegeo::Camera::GlobeCamera::GlobeCameraController& cameraController) :
-	m_trafficCongestionService(trafficCongestionService),
-	m_timeAccumulator(5.f),
-	m_congestionValue(static_cast<int>(Eegeo::TrafficCongestion::CongestionLevel::Light)),
-	m_key(Eegeo::Streaming::MortonKey::CreateFromString("01131232132001"))
-	,m_globeCameraStateRestorer(cameraController)
+        Eegeo::Camera::GlobeCamera::GlobeCameraController& cameraController)
+	: m_trafficCongestionService(trafficCongestionService)
+	, m_timeAccumulator(5.f)
+	, m_congestionValue(static_cast<int>(Eegeo::TrafficCongestion::CongestionLevel::Light))
+	, m_key(Eegeo::Streaming::MortonKey::CreateFromString("01131232132001"))
+	, m_globeCameraStateRestorer(cameraController)
 {
 
 }
 
 void TrafficCongestionExample::Suspend()
 {
-	m_trafficCongestionService.TrySetCongestionFor(m_key,
+	m_trafficCongestionService.SetCongestionFor(
+            m_key,
 	        RoadId,
 	        Eegeo::TrafficCongestion::CongestionLevel::Normal);
 }
@@ -38,15 +39,16 @@ void TrafficCongestionExample::Update(float dt)
 		Eegeo::TrafficCongestion::CongestionLevel::CongestionLevelValues congestionLevel =
 		    static_cast<Eegeo::TrafficCongestion::CongestionLevel::CongestionLevelValues>(m_congestionValue);
 
-		bool success = m_trafficCongestionService.TrySetCongestionFor(m_key,
-		               RoadId,
-		               congestionLevel);
+		m_trafficCongestionService.SetCongestionFor(
+                m_key,
+                RoadId,
+                congestionLevel);
 
-		Eegeo_TTY("%s congestion level %s on road id %d for morton key %s\n",
-		          success ? "Successfully set" : "Failed to set",
-		          congestionLevelNames[m_congestionValue].c_str(),
-		          RoadId,
-		          m_key.ToString().c_str());
+		Eegeo_TTY(
+                "Congestion level %s set on road id %d for morton key %s\n",
+                congestionLevelNames[m_congestionValue].c_str(),
+                RoadId,
+                m_key.ToString().c_str());
 
 		if(++m_congestionValue > Eegeo::TrafficCongestion::CongestionLevel::Gridlock)
 		{
