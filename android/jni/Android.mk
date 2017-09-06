8918cc99d23ad91a0925306b602d6b81eea1852c
marksimpson82 <mark.simpson@eegeo.com>
2017-09-06 15:55:55 +0100

    Fix link error w/ -latomic
    This is needed for NDK 13 and later.
    
    Buddy: Calum L

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -60,7 +60,7 @@ include $(PREBUILT_STATIC_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := eegeo-sdk-samples
-LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2 -lz -lm
+LOCAL_LDLIBS := -llog -landroid -lEGL -lGLESv2 -lz -lm -latomic
 LOCAL_LDLIBS += -Wl,--stub-group-size=1000000
 LOCAL_STATIC_LIBRARIES := eegeo-sdk-lib png-lib curl-lib uv-lib ssl-lib crypto-lib http-parser-lib jpeg-lib turbojpeg-lib
 
